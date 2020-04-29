53587f3b73900b695c944d50490d3a4130459c80
Ruslan Ramazanov <ruslanramazanov@Dev-MacBook-Pro.local>
2020-04-29 11:30:55 +0300

    maps-mvp - circle was added to cart with placemark, with various coords and two colors

--- ReactNativeYandexMaps/ViewController.swift
+++ ReactNativeYandexMaps/ViewController.swift
@@ -5,25 +5,44 @@ import YandexMapKit
 class ViewController: UIViewController {
     @IBOutlet weak var mapView: YMKMapView!
     
-    let TARGET_LOCATION = YMKPoint(latitude: 55.676265, longitude: 37.519503)
+    let RESTAURANT_LOCATION = YMKPoint(latitude: 55.676265, longitude: 37.519503)
+    let ORDER_LOCATION = YMKPoint(latitude: 55.681648, longitude: 37.516156)
+    let COLOR_OBJECTS = [
+        "red": [
+           "stroke": UIColor(named: "Red")!,
+           "fill": UIColor(named: "TransparentRed")!
+        ],
+        "green": [
+           "stroke": UIColor(named: "Green")!,
+           "fill": UIColor(named: "TransparentGreen")!
+        ]
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
          mapView.mapWindow.map.move(
-               with: YMKCameraPosition(target: TARGET_LOCATION, zoom: 15, azimuth: 0, tilt: 0),
-               animationType: YMKAnimation(type: YMKAnimationType.smooth, duration: 0.4),
-               cameraCallback: nil)
-        
+            with: YMKCameraPosition(target: RESTAURANT_LOCATION, zoom: 15, azimuth: 0, tilt: 0),
+            animationType: YMKAnimation(type: YMKAnimationType.smooth, duration: 0.4),
+            cameraCallback: nil
+        )
         createMapObjects()
     }
     
-    func createMapObjects() {
+    func createMapMarkerWithCircle (color: String) {
         let mapObjects = mapView.mapWindow.map.mapObjects
         
-        let placemark = mapObjects.addPlacemark(with: TARGET_LOCATION)
+        var location = RESTAURANT_LOCATION
+        var iconName = "RedPin"
+        
+        if (color == "green") {
+            location = ORDER_LOCATION
+            iconName = "GreenPin"
+        }
+        
+        let placemark = mapObjects.addPlacemark(with: location)
         placemark.opacity = 1
-        placemark.setIconWith(UIImage(named:"Mark")!, style: YMKIconStyle(
+        placemark.setIconWith(UIImage(named: iconName)!, style: YMKIconStyle(
             anchor: CGPoint(x: 0.5, y: 1) as NSValue,
             rotationType: YMKRotationType.noRotation.rawValue as NSNumber,
             zIndex: 0,
@@ -33,5 +52,17 @@ class ViewController: UIViewController {
             tappableArea: nil
         ))
 
+        let circle = mapObjects.addCircle(
+           with: YMKCircle(center: location, radius: 100),
+           stroke: COLOR_OBJECTS[color]!["stroke"]!,
+           strokeWidth: 2,
+           fill: COLOR_OBJECTS[color]!["fill"]!
+        )
+        circle.zIndex = 100
+    }
+    
+    func createMapObjects() {
+        createMapMarkerWithCircle(color: "red")
+        createMapMarkerWithCircle(color: "green")
     }
 }
